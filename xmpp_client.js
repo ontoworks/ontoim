91de8d396a30e18afbadec1059a69f7fe0a2713e
santiago <sgaviria@gmail.com>
2011-02-10 11:55:43 -0500

    buddy-list: scrolling

--- xmpp_client.js
+++ xmpp_client.js
@@ -1,14 +1,27 @@
 var sys = require('sys');
-var xmpp = require('../lib/node-xmpp');
+var xmpp = require('node-xmpp');
 var jQuery= require('jquery');
 
 var XmppClient= function() {
 };
 
-XmppChatClient.prototype.connect= function() {
+var google= new GoogleChat({username: 'sgaviria@gmail.com', password: 'S4ntiag0'});
+
+XmppChatClient.prototype.connect= function(o) {
+    var h = new xmpp.Client({ jid: o.username,
+			      password: o.password,
+			      host: o.host,
+			      port: o.port
+			    });
+
+    this.connection= h;
 };
 
 XmppChatClient.prototype.getBuddyList= function() {
+    var iq_roster= new xmpp.Element('iq', {type: 'get'});
+    var query_roster= new xmpp.Element('query', {xmlns:'jabber:iq:roster'});
+    iq_roster.cnode(query_roster);
+    this.connection.send(iq_roster);
 };
 
 XmppChatClient.prototype.sendMessage= function() {
@@ -16,6 +29,3 @@ XmppChatClient.prototype.sendMessage= function() {
 
 XmppChatClient.prototype.receiveMessage= function() {
 };
-
-
-
